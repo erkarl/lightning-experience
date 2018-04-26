61277d87f3920b8e9245bcbbf88d100e6aaf8c74
Karl Ranna <karl@karlranna.com>
2018-04-26 18:52:07 +0300

    Trigger invoiceFound from invoice-detector

--- src/content/src/invoice-detector/index.js
+++ src/content/src/invoice-detector/index.js
@@ -8,22 +8,19 @@ const MUTATION_OBSERVER_CONFIG = {
   subtree: true,
 };
 
-const domMutationDetected = (mutationsList) => {
-  console.log('domMutationDetected', mutationsList);
-  const invoices = mutationsList
-    .filter((mutation) => {
-      return mutation
-        && mutation.target
-        && mutation.target.textContent
-        && matchesInvoiceCode(mutation.target.textContent);
-    })
-    .map(cleanInvoice);
-  console.log('invoices', invoices.length);
-};
-
 export const searchForInvoices = ({invoiceFound}) => {
-  console.log('searchForInvoices', TARGET_ELEMENT);
-  const mutationObserver = new MutationObserver(domMutationDetected);
+  const mutationObserver = new MutationObserver((mutationsList) => {
+    const invoices = mutationsList
+      .filter((mutation) => {
+        return mutation
+          && mutation.target
+          && mutation.target.textContent
+          && matchesInvoiceCode(mutation.target.textContent);
+      })
+      .map((mutation) => mutation.target.textContent)
+      .map(cleanInvoice);
+    invoices.forEach(invoiceFound);
+  });
   mutationObserver.observe(TARGET_ELEMENT, MUTATION_OBSERVER_CONFIG);
   return mutationObserver;
 };
