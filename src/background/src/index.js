dcd036628e72cb02ac5707c2e1be21ed0f53b2ab
Karl Ranna <karl@karlranna.com>
2018-04-27 18:51:26 +0300

    Make payInvoice async and add ESLint

--- src/background/src/index.js
+++ src/background/src/index.js
@@ -3,53 +3,54 @@ console.log('Starting background bundle');
 const LND_RESTLISTEN = 'https://localhost:8081';
 const TRANSACTIONS_ENDPOINT = '/v1/channels/transactions';
 
-const payInvoice = (invoiceCode) => {
-  const body = {
-    "payment_request": invoiceCode
-  };
+const payInvoiceRequest = (body) => {
   return fetch(`${LND_RESTLISTEN}${TRANSACTIONS_ENDPOINT}`, {
     method: 'POST',
     body: JSON.stringify(body),
     headers: {
-      "Content-Type": "application/json",
+      'Content-Type': 'application/json',
     },
-  })
-    .then((response) => {
-      console.log('response is', response);
-      if (!response.ok) {
-        return Promise.reject('failed_to_pay_invoice');
-      }
-      return Promise.resolve(response.json());
-    }, (e) => {
-      console.error('Failed to fetch: ', e);
-    });
+  });
+};
+
+const payInvoice = async (invoiceCode) => {
+  const body = {
+    'payment_request': invoiceCode
+  };
+  const response = await payInvoiceRequest(body);
+  console.log('response is', response);
+  if (!response.ok) {
+    return Promise.reject('failed_to_pay_invoice');
+  }
+  return Promise.resolve(response.json());
 };
 
 console.log('Adding external message listener...');
-chrome.runtime.onMessage.addListener(function(request, sender) {
-    if (request.type === 'pay_invoice') {
-      payInvoice(request.options.invoiceCode)
-        .then((lndResponse) => {
-          console.log('lndResponse is', lndResponse);
-        }).then(() => {
-          // TODO: This assumes invoice always gets paid and that nothing can wrong :P.
-          console.log('Invoice has been paid.');
-        });
-    }
-    if (request.type == "notification") {
-      // TODO: Actually decode the invoice.
-      const decodedInvoice = {
-        description: 'Payment description.',
-        amount: 150,
-      };
-      chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-        console.log('Sending message to gateway.', tabs);
-        if (tabs.length > 0) {
-          chrome.tabs.sendMessage(tabs[0].id, {
-            action: "confirm_decoded_invoice",
-            decodedInvoice,
-          }, (response) => {});
-        }
+chrome.runtime.onMessage.addListener((request/*, sender */) => {
+  // TODO: Make sure sender is whitelisted and reject by default.
+  if (request.type === 'pay_invoice') {
+    payInvoice(request.options.invoiceCode)
+      .then((lndResponse) => {
+        console.log('lndResponse is', lndResponse);
+      }).then(() => {
+        // TODO: This assumes invoice always gets paid and that nothing can wrong :P.
+        console.log('Invoice has been paid.');
       });
-    }
+  }
+  if (request.type == 'notification') {
+    // TODO: Actually decode the invoice.
+    const decodedInvoice = {
+      description: 'Payment description.',
+      amount: 150,
+    };
+    chrome.tabs.query({active: true, currentWindow: true}, (tabs) => {
+      console.log('Sending message to gateway.', tabs);
+      if (tabs.length > 0) {
+        chrome.tabs.sendMessage(tabs[0].id, {
+          action: 'confirm_decoded_invoice',
+          decodedInvoice,
+        }, () => {});
+      }
+    });
+  }
 });
