72c0e904bd6166d1dd6ec390ee948489dadde612
Karl Ranna <karl@karlranna.com>
2018-04-27 19:18:50 +0300

    Clean up index

--- src/background/src/index.js
+++ src/background/src/index.js
@@ -3,16 +3,13 @@ import { payInvoice, decodeInvoice } from './lnd-rest/requests';
 console.log('Starting background bundle');
 
 console.log('Adding external message listener...');
-chrome.runtime.onMessage.addListener(async (request/*, sender */) => {
+
+const onMessage = async (request/*, sender */) => {
   // TODO: Make sure sender is whitelisted and reject by default.
   if (request.type === 'pay_invoice') {
-    payInvoice(request.options.invoiceCode)
-      .then((lndResponse) => {
-        console.log('lndResponse is', lndResponse);
-      }).then(() => {
-        // TODO: This assumes invoice always gets paid and that nothing can wrong :P.
-        console.log('Invoice has been paid.');
-      });
+    await payInvoice(request.options.invoiceCode);
+    // TODO: This assumes invoice always gets paid and that nothing can wrong :P.
+    console.log('Invoice has been paid.');
   }
   if (request.type == 'notification') {
     const invoiceCode = request.options.invoiceCode;
@@ -22,7 +19,6 @@ chrome.runtime.onMessage.addListener(async (request/*, sender */) => {
       amount: decodedPayReq.num_satoshis,
     };
     chrome.tabs.query({active: true, currentWindow: true}, (tabs) => {
-      console.log('Sending message to gateway.', tabs);
       if (tabs.length > 0) {
         chrome.tabs.sendMessage(tabs[0].id, {
           action: 'confirm_decoded_invoice',
@@ -31,4 +27,6 @@ chrome.runtime.onMessage.addListener(async (request/*, sender */) => {
       }
     });
   }
-});
+};
+
+chrome.runtime.onMessage.addListener(onMessage);
