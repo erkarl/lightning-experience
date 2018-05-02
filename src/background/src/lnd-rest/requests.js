28901ce15a984cc84f0d757bf7d0fca838d5416c
Karl Ranna <karl@karlranna.com>
2018-05-02 10:43:26 +0300

    Add support for hardcoded macaroons

--- src/background/src/lnd-rest/requests.js
+++ src/background/src/lnd-rest/requests.js
@@ -1,23 +1,27 @@
 const LND_RESTLISTEN = 'https://localhost:8081';
 const TRANSACTIONS_ENDPOINT = '/v1/channels/transactions';
 const PAY_REQ_ENDPOINT = '/v1/payreq';
+const ADMIN_MACAROON_HEX = '0201036c6e6402bb01030a106f5958baca6a8a0c0d14cd71a7701e461201301a160a0761646472657373120472656164120577726974651a130a04696e666f120472656164120577726974651a170a08696e766f69636573120472656164120577726974651a160a076d657373616765120472656164120577726974651a170a086f6666636861696e120472656164120577726974651a160a076f6e636861696e120472656164120577726974651a140a0570656572731204726561641205777269746500000620d63e09ed4e2701c9996734220b3c49956a0b99a78aa090a50c833e2badddc931';
+
+const getHeaders = () => {
+  return {
+    'Content-Type': 'application/json',
+    'Grpc-Metadata-macaroon': ADMIN_MACAROON_HEX,
+  };
+};
 
 const payInvoiceRequest = (body) => {
   return fetch(`${LND_RESTLISTEN}${TRANSACTIONS_ENDPOINT}`, {
     method: 'POST',
     body: JSON.stringify(body),
-    headers: {
-      'Content-Type': 'application/json',
-    },
+    headers: getHeaders(),
   });
 };
 
 const decodeInvoiceRequest = (payReq) => {
   return fetch(`${LND_RESTLISTEN}${PAY_REQ_ENDPOINT}/${payReq}`, {
     method: 'GET',
-    headers: {
-      'Content-Type': 'application/json',
-    },
+    headers: getHeaders(),
   });
 };
 
