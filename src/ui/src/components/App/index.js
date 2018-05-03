6b6bfd004920455feb3c37b0dc0fbccd13077350
Karl Ranna <karl@karlranna.com>
2018-05-03 15:05:00 +0300

    Add UI for setting restlisten and macaroon

--- src/ui/src/components/App/index.js
+++ src/ui/src/components/App/index.js
@@ -1,14 +1,72 @@
 import React, { Component } from 'react';
 import {
   App,
+  Label,
+  Input,
+  Button,
 } from './styles';
 
 class AppComponent extends Component {
 
+  constructor(props) {
+    super(props);
+    this.state = {
+      restlisten: 'localhost:8080',
+      hexMacaroon: '',
+    };
+  }
+
+  saveSettings() {
+    const hexMacaroon = this.state.hexMacaroon;
+    const restlisten = this.state.restlisten;
+    console.log('saveSettings', restlisten, hexMacaroon);
+  }
+
+  updateRestlisten(e) {
+    const restlisten = e.target.value;
+    this.setState({...this.state, restlisten})
+  }
+
+  updateMacaroon(e) {
+    // TODO: Add error catching.
+    const file = e.target.files[0];
+    const fileReader = new FileReader();
+    const buf2hex = (buffer) => {
+      return Array.prototype.map.call(new Uint8Array(buffer), x => ('00' + x.toString(16)).slice(-2)).join('');
+    };
+    fileReader.readAsArrayBuffer(file);
+    fileReader.onload = () => {
+      const hexMacaroon = buf2hex(fileReader.result);
+      this.setState({...this.state, hexMacaroon})
+    };
+  }
+
   render() {
     return (
       <App>
-        <h1>React Started</h1>
+        <h1>Settings</h1>
+        <hr />
+        <div>
+          <Label htmlFor="restlisten">
+            LND restlisten:
+          </Label>
+          <Input
+            type="text"
+            name="restlisten"
+            placeholder="localhost:8080"
+            onChange={this.updateRestlisten.bind(this)}
+            value={this.state.restlisten}
+          />
+          <Label htmlFor="macaroon">LND macaroon: </Label>
+          <Input
+            type="file"
+            name="macaroon"
+            onChange={this.updateMacaroon.bind(this)}
+          />
+          <Button onClick={this.saveSettings.bind(this)}>
+            Save
+          </Button>
+        </div>
       </App>
     );
   }
--- src/ui/src/components/App/styles.js
+++ src/ui/src/components/App/styles.js
@@ -1,4 +1,19 @@
 import styled from 'styled-components';
 
 export const App = styled.div`
+  min-width: 30rem;
+`;
+
+export const Label = styled.label`
+  display: block;
+  width: 100%;
+  margin: 1rem 0 1rem 0;
+`;
+
+export const Input = styled.input`
+  width: 100%;
+`;
+
+export const Button = styled.button`
+  margin-top: 1rem;
 `;
